b4380a6d63e264eaec4a09c3597d3cec68c5b425
0tae <choiyt3465@naver.com>
2025-08-13 15:51:25 +0900

    init : 프로젝트 초기화

---
+++ src/main/java/com/example/hatsalvoids/HatsalvoidsApplication.java
@@ -0,0 +1,13 @@
+package com.example.hatsalvoids;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HatsalvoidsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HatsalvoidsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/hatsalvoids/HatsalvoidsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hatsalvoids;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HatsalvoidsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
