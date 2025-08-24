829dd236f60b5cf40e80c18b25d10743aeafaca4
0tae <choiyt3465@naver.com>
2025-08-24 17:10:08 +0900

    refactor : apiResponse external 패키지 이동

--- src/main/java/com/example/hatsalvoids/external/model/response/BuildingSpecificsByKeywordApiResponse.java
+++ src/main/java/com/example/hatsalvoids/external/model/response/BuildingSpecificsByKeywordApiResponse.java
@@ -1,28 +1,48 @@
-package com.example.hatsalvoids.building.model;
+package com.example.hatsalvoids.external.model.response;
 
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 import java.util.List;
-import java.util.Map;
 
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
-public class BuildingSpecificByKeywordApiResponse {
+@ToString
+public class BuildingSpecificsByKeywordApiResponse {
     List<Documents> documents;
-    static class Documents {
+
+    @Getter
+    @ToString
+    static public class Documents {
+
+        @JsonProperty("address_name")
         private String addressName;
+
+        @JsonProperty("category_group_code")
         private String categoryGroupCode;
+
+        @JsonProperty("category_group_name")
         private String categoryGroupName;
+
+        @JsonProperty("category_name")
         private String categoryName;
+
         private String distance;
         private String id;
         private String phone;
+
+        @JsonProperty("place_name")
         private String placeName;
+
+        @JsonProperty("place_url")
         private String placeUrl;
+
+        @JsonProperty("road_address_name")
         private String roadAddressName;
         private String x;
         private String y;
